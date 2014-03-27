0f138cb4e85f7a7c715c61b01c08d55e9ff5558a
Priyesh Patel <priyesh.96@hotmail.com>
2014-03-27 17:21:30 -0400

    Add bravo

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -18,6 +18,9 @@ endif
 ifeq (pa_a700,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_a700.mk
 endif
+ifeq (pa_bravo,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_bravo.mk
+endif
 ifeq (pa_d2lte,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_d2lte.mk
 endif
